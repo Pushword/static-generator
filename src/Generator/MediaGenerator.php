4459e7fffa64620bce1bab653829441dea92df82
RobinDev <contact@piedweb.com>
2021-09-03 21:03:06 +0000

    fix oups error

--- src/Generator/MediaGenerator.php
+++ src/Generator/MediaGenerator.php
@@ -19,7 +19,7 @@ class MediaGenerator extends AbstractGenerator
     protected function copyMediaToDownload()
     {
         $publicMediaDir = $this->params->get('pw.public_media_dir');
-        $mediaDir = $this->params->get('pw.media_dir');
+        $mediaDir = (string) $this->params->get('pw.media_dir');
         $staticMediaDir = $this->getStaticDir().'/'.$publicMediaDir;
 
         $symlink = $this->mustSymlink();
