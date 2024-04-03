26c7f21f585ea110509f395fbafef639cc93a746
RobinDev <contact@piedweb.com>
2024-04-03 08:30:49 +0000

    prepare v1

--- install.php
+++ install.php
@@ -1,12 +1,15 @@
 <?php
 
-/**
- * Execute via Pushword\Installer\PostInstall::postUpdateCommand
- */
+use Pushword\Installer\PostInstall;
 
-if (!isset($postInstallRunning)) return;if (! \Pushword\Installer\PostInstall::isRoot()) {
+/**
+ * Execute via Pushword\Installer\PostInstall::postUpdateCommand.
+ */
+if (! isset($postInstallRunning)) {
+    return;
+}if (! PostInstall::isRoot()) {
     throw new Exception('installer mus be run from root');
 }
 
 echo '~~ Adding Routes'.chr(10);
-\Pushword\Installer\PostInstall::addOnTop('config/routes.yaml', "static:\n    resource: '@PushwordStaticGeneratorBundle/StaticRoutes.yaml'\n");
+PostInstall::addOnTop('config/routes.yaml', "static:\n    resource: '@PushwordStaticGeneratorBundle/StaticRoutes.yaml'\n");
